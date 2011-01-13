19edc600155446efe13f214ba8ce8c288bc22e98
Tim Freeman <tim@timfreeman.org>
2011-01-13 14:50:19 -0600

    rough draft of sample epu launch

---
+++ sandbox/sleeper/README.txt
@@ -0,0 +1,8 @@
+Edit the "provisioner/cei_environment" file (add the correct credentials).
+
+Then run:
+
+$ cp chefsolo.sh provisioner/run.sh
+$ tar cvf provisioner.tar.gz provisioner/
+
+Then proceed with launching "main.conf"
---
+++ sandbox/sleeper/chefsolo.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+if [ ! -d /opt ]; then 
+  sudo mkdir /opt; 
+  if [ $? -ne 0 ]; then
+      exit 1
+  fi
+fi
+
+if [ -d /opt/dt-data ]; then
+  (cd /opt/dt-data && sudo git fetch)
+  if [ $? -ne 0 ]; then
+      exit 1
+  fi
+else
+  (cd /opt && sudo git clone https://github.com/ooici/dt-data.git)
+  if [ $? -ne 0 ]; then
+      exit 1
+  fi
+fi
+
+(cd /opt/dt-data && sudo git reset --hard origin/HEAD)
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+echo "Retrieved the dt-data repository, HEAD is currently:"
+(cd /opt/dt-data && sudo git rev-parse HEAD)
+echo ""
+
+sudo mkdir -p /opt/dt-data/run
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+sudo mv bootconf.json /opt/dt-data/run/chefroles.json
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+if [ -f provisioner/cei_environment ]; then
+    sudo mv provisioner/cei_environment /opt/cei_environment
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
+    chmod 400 /opt/cei_environment
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
+fi
+    
+cat >> chefconf.rb << "EOF"
+cookbook_path "/opt/dt-data/cookbooks"
+log_level :info
+file_store_path "/opt/dt-data/tmp"
+file_cache_path "/opt/dt-data/tmp"
+Chef::Log::Formatter.show_time = false
+
+EOF
+
+sudo mv chefconf.rb /opt/dt-data/run/chefconf.rb
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+echo "Running chef-solo"
+
+sudo chef-solo -l debug -c /opt/dt-data/run/chefconf.rb -j /opt/dt-data/run/chefroles.json
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+
