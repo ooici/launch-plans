d7f372e942a696ef877d004a479ce8f081ac6ea5
Jamie Chen <jachen@ucsd.edu>
2011-05-20 15:45:30 -0700

    Add level 7

---
+++ R1/r1c3/07/lv7ready.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# We will have to make something that can automically determine a list of EPU
+# controllers to wait for in each level.  For now, hardcoding the name here.
+
+CONTROLLER="identityregistry_epu_controller"
+
+APP_DIR="/home/epu7/app"
+MSG_CONF = APP_DIR + "/messaging.conf"
+
+VENV_PYTHON= APP_DIR + "/bin/python"
+
+import os
+import subprocess
+import sys
+
+run = [VENV_PYTHON, "./scripts/epu-state-wait", MSG_CONF, CONTROLLER]
+runcmd = ' '.join(run)
+print runcmd
+retcode = subprocess.call(runcmd, shell=True, cwd=APP_DIR, stderr=subprocess.STDOUT)
+
+if retcode:
+    print "Problem waiting for EPU controller stable state for '%s'" % CONTROLLER
+sys.exit(retcode)
