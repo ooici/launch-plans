516ba04830e7301f710cf097dea006b2aeded956
Tim Freeman <tim@timfreeman.org>
2011-05-20 19:09:55 -0500

    Cleanup, making level 10 an EPU for EOI agent, and moving ingestion test to 11

--- R1/r1c3/10/lv10ready.py
+++ R1/r1c3/10/lv10ready.py
@@ -1,40 +1,24 @@
 #!/usr/bin/env python
 
-TRIAL_TEST="tests.services.dm.test_ingestion"
+# We will have to make something that can automically determine a list of EPU
+# controllers to wait for in each level.  For now, hardcoding the name here.
 
-APP_DIR="/home/cc/app"
+CONTROLLER="ingestion_epu_controller"
+
+APP_DIR="/home/epu9/app"
+MSG_CONF = APP_DIR + "/messaging.conf"
+
+VENV_PYTHON= APP_DIR + "/bin/python"
 
 import os
 import subprocess
 import sys
 
-ooiciconn = os.path.join(APP_DIR, "ooici-conn.properties")
-if not os.path.exists(ooiciconn):
-    raise Exception("Could not find file: %s" % ooiciconn)
-
-exchange = None
-server = None
-
-f = open(ooiciconn, 'r')
-for line in f.readlines():
-    if line.rfind("=") >= 1:
-        (key, value) = line.split("=")
-        if key == "exchange":
-            exchange = value.strip()
-        elif key == "server":
-            server = value.strip()
-f.close()
-
-if not exchange and not server:
-    raise Exception("Could not find all necessary configurations in order to run trial tests")
-        
-envmap = {"ION_TEST_CASE_SYSNAME": exchange}
-
-run = ["./bin/trial", TRIAL_TEST]
+run = [VENV_PYTHON, "./scripts/epu-state-wait", MSG_CONF, CONTROLLER]
 runcmd = ' '.join(run)
 print runcmd
-retcode = subprocess.call(runcmd, shell=True, env=envmap, cwd=APP_DIR, stderr=subprocess.STDOUT)
+retcode = subprocess.call(runcmd, shell=True, cwd=APP_DIR, stderr=subprocess.STDOUT)
 
 if retcode:
-    print "Problem running trial test: %s" % TRIAL_TEST
+    print "Problem waiting for EPU controller stable state for '%s'" % CONTROLLER
 sys.exit(retcode)
