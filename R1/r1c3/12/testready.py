cf0001af04e46e5cec6ddca6f58cb6ddfa8c7053
Jamie Chen <jachen@ucsd.edu>
2011-08-12 15:32:45 -0700

    Fix level 12

---
+++ R1/r1c3/12/testready.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+TRIAL_TEST="itv_tests.services.dm.test_ingestion.IntTestIngest"
+
+APP_DIR="/home/test/app"
+
+import os
+import subprocess
+import sys
+
+ooiciconn = os.path.join(APP_DIR, "ooici-conn.properties")
+if not os.path.exists(ooiciconn):
+    raise Exception("Could not find file: %s" % ooiciconn)
+
+exchange = None
+server = None
+
+f = open(ooiciconn, 'r')
+for line in f.readlines():
+    if line.rfind("=") >= 1:
+        (key, value) = line.split("=")
+        if key == "exchange":
+            exchange = value.strip()
+        elif key == "server":
+            server = value.strip()
+f.close()
+
+if not exchange and not server:
+    raise Exception("Could not find all necessary configurations in order to run trial tests")
+        
+envmap = {"ION_TEST_CASE_SYSNAME": exchange}
+
+run = ["./bin/trial", TRIAL_TEST]
+runcmd = ' '.join(run)
+print runcmd
+retcode = subprocess.call(runcmd, shell=True, env=envmap, cwd=APP_DIR, stderr=subprocess.STDOUT)
+
+if retcode:
+    print "Problem running trial test: %s" % TRIAL_TEST
+sys.exit(retcode)
