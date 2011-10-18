184964202fba30f3b25195abe0b37ed355372487
Jamie Chen <jachen@ucsd.edu>
2011-10-18 14:31:29 -0700

    Add R2E1 launch plan

---
+++ R2/r2e1/04/lv4ready.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# We will have to make something that can automically determine a list of EPU
+# controllers to wait for in each level.  For now, hardcoding the name here.
+
+CONTROLLER="lv4_epu_controller"
+
+APP_DIR="/home/epu4/app"
+MSG_CONF = APP_DIR + "/messaging.conf"
+
+VENV_PYTHON="/home/epu4/app/bin/python"
+VENV_TRIAL="/home/epu4/app/bin/trial"
+
+import os
+import subprocess
+import sys
+
+run = [VENV_PYTHON, "./scripts/epu-state-wait", MSG_CONF, CONTROLLER]
+runcmd = ' '.join(run)
+print runcmd
+
+exitcode = -1
+while exitcode < 0:
+    retcode = subprocess.call(runcmd, shell=True, cwd=APP_DIR, stderr=subprocess.STDOUT)
+    if not retcode:
+        exitcode = 0
+    elif retcode == 123:
+        exitcode = 123
+        print "Problem waiting for EPU controller stable state for '%s'" % CONTROLLER
+    else:
+        print "epu-state-wait exited with %d, running again." % retcode
+sys.exit(exitcode)
---
+++ R2/r2e1/README.txt
@@ -0,0 +1,7 @@
+See the documentation here: 
+
+   http://www.nimbusproject.org/doc/epu/latest/
+   
+In particular the OOICI launch plan reference:
+
+   http://www.nimbusproject.org/doc/epu/latest/ooi.html
