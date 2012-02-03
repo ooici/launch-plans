1b0827e3d0341d75fc59fe7da390fab7a5d332e0
Patrick Armstrong <patricka@uvic.ca>
2012-02-03 15:36:30 -0800

    Work around problem in launch plan termination, and bump pyon slots

--- sandbox/lightweight/level1-local/basenode-epu-harness.sh
+++ sandbox/lightweight/level1-local/basenode-epu-harness.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
+. bootenv.sh
 
 ERROR=1
-USAGE="usage: $0 path/to/virtualenv start|stop|status"
+USAGE="usage: $0 start|stop|status"
 
 
 if [ -z "$1" ]; then
-    echo "Your virtualenv must be the first argument"
-    echo $USAGE
-    exit $ERROR
-fi
-VENV="$1"
-
-if [ -z "$2" ]; then
     echo "You must specify whether to start or stop"
     echo $USAGE
     exit $ERROR
 fi
-ACTION="$2"
+ACTION="$1"
 
-if [ -z "$3" ]; then
-    echo "You must specify an exchange scope as your third argument"
-    echo $USAGE
-    exit $ERROR
-fi
-EXCHANGE="$3"
+VENV="${virtualenv}"
+EXCHANGE="${exchange_scope}"
 
 ACTIVATE="${VENV}/bin/activate"
 
