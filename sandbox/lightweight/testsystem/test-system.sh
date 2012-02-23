afacbf266c46b6c10125e9120c007b7ead3da869
Jamie Chen <jachen@ucsd.edu>
2012-02-22 16:33:14 -0800

    Edit test script

--- sandbox/lightweight/testsystem/test-system.sh
+++ sandbox/lightweight/testsystem/test-system.sh
@@ -7,4 +7,7 @@ set -e
 echo "It works!"
 echo "Pyon path is $pyon_path" 
 
-exit 0
+export CEI_LAUNCH_TEST=1
+cd $pyon_path
+./bin/pycc -x ion.processes.bootstrap.datastore_loader.DatastoreLoader op=dump path=res/dd
+./bin/nosetests -a INT,group=sa -v
