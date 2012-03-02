7a7bcc125d7636d2cbd01510da1d3926d4f7d1c5
Patrick Armstrong <patricka@uvic.ca>
2012-03-02 14:36:51 -0800

    Fix type in testsystem script

--- sandbox/lightweight/testsystem/testsystem.sh
+++ sandbox/lightweight/testsystem/testsystem.sh
@@ -11,5 +11,5 @@ export CEI_LAUNCH_TEST=1
 cd $pyon_path
 env
 ./bin/pycc -x ion.processes.bootstrap.datastore_loader.DatastoreLoader op=dump path=res/dd
-./bin/nosetests -a INT,group=sa -v 2>&1 | tee $pycc_path/logs/nose.log
+./bin/nosetests -a INT,group=sa -v 2>&1 | tee $pyon_path/logs/nose.log
 env
