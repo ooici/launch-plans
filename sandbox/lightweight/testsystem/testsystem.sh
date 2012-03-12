e31f1aab60e9d35f919388dfd298833e5c8c1b64
Jamie Chen <jachen@ucsd.edu>
2012-03-12 09:58:30 -0700

    Add loader int test

--- sandbox/lightweight/testsystem/testsystem.sh
+++ sandbox/lightweight/testsystem/testsystem.sh
@@ -10,6 +10,6 @@ echo "Pyon path is $pyon_path"
 export CEI_LAUNCH_TEST=1
 cd $pyon_path
 ./bin/pycc -x ion.processes.bootstrap.datastore_loader.DatastoreLoader op=dump path=res/dd
-./bin/nosetests -a INT,group=dm -v 2>&1 | tee $pyon_path/logs/nose.log
+./bin/nosetests -A "INT and group in ['loader','dm']" -v 2>&1 | tee $pyon_path/logs/nose.log
 
 exit $?
