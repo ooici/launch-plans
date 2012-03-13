f8862099e47fb30c0e3e5449454c8ff282148075
Jamie Chen <jachen@ucsd.edu>
2012-03-13 09:59:56 -0700

    dm tests for now

--- sandbox/lightweight/testsystem/testsystem.sh
+++ sandbox/lightweight/testsystem/testsystem.sh
@@ -10,6 +10,7 @@ echo "Pyon path is $pyon_path"
 export CEI_LAUNCH_TEST=1
 cd $pyon_path
 ./bin/pycc -x ion.processes.bootstrap.datastore_loader.DatastoreLoader op=dump path=res/dd
-./bin/nosetests -A "INT and group in ['loader','dm']" -v 2>&1 | tee $pyon_path/logs/nose.log
+# ./bin/nosetests -A "INT and group in ['loader','dm']" -v 2>&1 | tee $pyon_path/logs/nose.log
+./bin/nosetests -A "INT and group in ['dm']" -v 2>&1 | tee $pyon_path/logs/nose.log
 
 exit $?
