1317ee05babd054de95dd07c0a982dad36f71e58
Jamie Chen <jachen@ucsd.edu>
2012-03-13 14:28:12 -0700

    test loader for now

--- sandbox/lightweight/testsystem/testsystem.sh
+++ sandbox/lightweight/testsystem/testsystem.sh
@@ -11,6 +11,7 @@ export CEI_LAUNCH_TEST=1
 cd $pyon_path
 ./bin/pycc -x ion.processes.bootstrap.datastore_loader.DatastoreLoader op=dump path=res/dd
 # ./bin/nosetests -A "INT and group in ['loader','dm']" -v 2>&1 | tee $pyon_path/logs/nose.log
-./bin/nosetests -A "INT and group in ['dm']" -v 2>&1 | tee $pyon_path/logs/nose.log
+# ./bin/nosetests -A "INT and group in ['dm']" -v 2>&1 | tee $pyon_path/logs/nose.log
+./bin/nosetests -A "INT and group in ['loader']" -v 2>&1 | tee $pyon_path/logs/nose.log
 
 exit $?
