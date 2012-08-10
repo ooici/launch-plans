42f646117d64958f2ecac810a0c312bf13880d94
Patrick Armstrong <patricka@uvic.ca>
2012-08-09 17:05:16 -0700

    Add pyon-bootstrap level

---
+++ sandbox/lightweight/pyon-bootstrap/pyon-bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set +e
+. bootenv.sh
+set -e
+
+if [ -n "$1" ]; then
+    PYON_PATH=$1
+else
+    PYON_PATH="/home/${epu_username}/coi-services/"
+fi
+
+CONFIG=`pwd`/bootconf.json
+
+#do pyon cleanup
+cd $PYON_PATH
+./bin/pycc -X system.auto_bootstrap=True --config $CONFIG
